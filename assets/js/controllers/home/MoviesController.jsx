9cad4af8bc427aa92aa1c908136577f26f9ae534
Ryan Webber <ryan@sequence.com>
2016-04-01 16:20:27 -0700

    Added styling of homepage

---
+++ assets/js/controllers/home/MoviesController.jsx
@@ -0,0 +1,65 @@
+define(['react', 'jquery', 'app/controllers/home/MovieItem'],
+    function (React, $, MovieItem) {
+
+        var MoviesController = React.createClass({
+            getInitialState: function() {
+                return {
+                    movies: [],
+                    loading: true,
+                    error: null
+                };
+            },
+            setLoading: function(val){
+                var state = this.state;
+                state.loading = val;
+                this.setState(state);
+            },
+            componentDidMount: function() {
+                // TODO stuff
+                $.get("/movie").done(function(movies){
+                    this.setState({movies: movies});
+                }.bind(this)).fail(function(err){
+                    this.setState({error: "Couldn't Load Movies"});
+                }.bind(this)).always(function(){
+                    this.setState({loading: false});
+                }.bind(this));
+            },
+            componentWillUnmount: function(){
+                // TODO stuff
+            },
+            render: function(){
+                var movies = this.state.movies;
+
+                if(this.state.loading === true){
+                    return(
+                        <div>
+                            <h1>Movies</h1>
+                            <hr/>
+                            <div className="movie-container">
+                                <div className="spinner center--all"></div>
+                            </div>
+                        </div>
+                    );
+                }else{
+                    var movies = movies.map(function(movie, i){
+                        return(
+                            <MovieItem movie={movie} key={movie.id + "-" + i}/>
+                        );
+                    }.bind(this));
+
+                    return(
+                        <div>
+                            <h1>Movies  <small>{movies.length} Titles</small></h1>
+                            <hr/>
+                            <div className="movie-container">
+                                {movies}
+                            </div>
+                        </div>
+                    );
+                }
+            }
+        });
+        return MoviesController;
+
+    }
+);
---
+++ assets/js/controllers/home/main.jsx
@@ -0,0 +1,25 @@
+requirejs.config({
+    paths: {
+      'react': '/bower_components/react/react-with-addons',
+      'reactdom': '/bower_components/react/react-dom',
+      'jquery': '/bower_components/jquery/dist/jquery',
+      'bootstrap': '/bower_components/bootstrap/dist/js/bootstrap.min.js',
+      'app': '/js'
+    },
+});
+
+require(['jquery', 'react', 'reactdom', 'app/controllers/home/MoviesController', 'app/controllers/home/ShowsController'],
+    function ($, React, ReactDOM, MoviesController, ShowsController) {
+        $(document).ready(function(){
+            ReactDOM.render(
+                <MoviesController />,
+                document.getElementById('movies')
+            );
+
+            ReactDOM.render(
+                <ShowsController />,
+                document.getElementById('shows')
+            );
+        });
+    }
+);
