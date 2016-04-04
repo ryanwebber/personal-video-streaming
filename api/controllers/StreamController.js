5e6e9ef3f85b308bcd4e19389b4c6d5caaed9010
Ryan Webber <ryan@sequence.com>
2016-04-04 11:49:25 -0700

    Removed fs access

--- api/controllers/StreamController.js
+++ api/controllers/StreamController.js
@@ -10,32 +10,26 @@ module.exports = {
 			id: video_id
 		}).then(function(video){
 			var path = video.file;
-			fs.access(path, fs.F_OK, function(err) {
-			    if (!err) {
-			        var stat = fs.statSync(path);
+			var stat = fs.statSync(path);
 
-			        var total = stat.size;
-					if (req.headers['range']) {
-						var range = req.headers.range;
-						var parts = range.replace(/bytes=/, "").split("-");
-						var partialstart = parts[0];
-						var partialend = parts[1];
+	        var total = stat.size;
+			if (req.headers['range']) {
+				var range = req.headers.range;
+				var parts = range.replace(/bytes=/, "").split("-");
+				var partialstart = parts[0];
+				var partialend = parts[1];
 
-						var start = parseInt(partialstart, 10);
-						var end = partialend ? parseInt(partialend, 10) : total-1;
-						var chunksize = (end-start)+1;
+				var start = parseInt(partialstart, 10);
+				var end = partialend ? parseInt(partialend, 10) : total-1;
+				var chunksize = (end-start)+1;
 
-						var file = fs.createReadStream(path, {start: start, end: end});
-						res.writeHead(206, { 'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
-						file.pipe(res);
-					} else {
-						res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'video/mp4' });
-						fs.createReadStream(path).pipe(res);
-					}
-			    } else {
-			        res.send(404);
-			    }
-			});
+				var file = fs.createReadStream(path, {start: start, end: end});
+				res.writeHead(206, { 'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
+				file.pipe(res);
+			} else {
+				res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'video/mp4' });
+				fs.createReadStream(path).pipe(res);
+			}
 		}).catch(function(err){
 			sails.log.info(err);
 			res.send(404)
