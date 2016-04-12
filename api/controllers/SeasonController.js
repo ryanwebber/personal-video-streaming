ead382d10cd03533e95f6a395ce821e33a82ca44
Ryan Webber <ryan551024@gmail.com>
2016-04-11 23:27:51 -0700

    Added most ui for show, fixed uploading

--- api/controllers/SeasonController.js
+++ api/controllers/SeasonController.js
@@ -6,6 +6,24 @@
  */
 
 module.exports = {
-	
+	prepare: function(req, res){
+		var show = req.body.show;
+		var seasonNumber = req.body.seasonNumber;
+
+		if(!show || !seasonNumber){
+			return res.send(400);
+		}
+
+		Season.findOrCreate({
+			show: show,
+			seasonNumber: seasonNumber
+		}).exec(function(err, season){
+			if(!err && season){
+				res.json(season);
+			}else{
+				res.send(500);
+			}
+		});
+	}
 };
 
--- assets/js/controllers/home/ShowItem.jsx
+++ assets/js/controllers/home/ShowItem.jsx
@@ -1,15 +1,49 @@
 define(['react', 'jquery', 'app/controllers/home/ModalController', 'app/dependencies/react-slick'], 
     function (React, $, ModalController, Carousel) {
 
+        var LeftCarouselButton = React.createClass({
+            render: function() {
+                return(
+                    <button {...this.props} className="carousel-button left">
+                        <i className="fa fa-chevron-circle-left center--all"></i>
+                    </button>
+                );
+            }
+        });
+
+        var RightCarouselButton = React.createClass({
+            render: function() {
+                return(
+                    <button {...this.props} className="carousel-button right">
+                        <i className="fa fa-chevron-circle-right center--all"></i>
+                    </button>
+                );  
+            }
+        });
+
         var ShowItem = React.createClass({
             getInitialState: function(){
                 return {
                     modalOpen: false,
-                    progress: null
+                    progress: null,
+                    activeSeason: 0,
+                    loadingSeasons: true,
+                    seasons: []
                 };
             },
             componentDidMount: function() {
                 io.socket.on('upload.progress', this.updateProgress);
+                $.get("/season", {
+                    show: this.props.show.id
+                }).done(function(seasons){
+                    this.setState({
+                        seasons: seasons
+                    });
+                }.bind(this)).always(function(){
+                    this.setState({
+                        loadingSeasons: false
+                    });
+                }.bind(this));
             },
             componentWillUnmount: function(){
                 io.socket.off('upload.progress', this.updateProgress);
@@ -27,6 +61,10 @@ define(['react', 'jquery', 'app/controllers/home/ModalController', 'app/dependen
                     this.setState({progress: progress});
                 }
             },
+            linkForEpisode: function(episode){
+                var base = "/watch/";
+                return base + episode.video;
+            },
             render: function(){
                 var show = this.props.show;
 
@@ -40,32 +78,51 @@ define(['react', 'jquery', 'app/controllers/home/ModalController', 'app/dependen
                 	str += "s";
                 }
 
-                if(show.seasons.length > 0){
+                if(this.state.loadingSeasons){
+                    var episodeElem = (
+                        <div className="spinner center--all"></div>
+                    );
+                }else if(show.seasons.length > 0){
 
                 	var settings = {
 						dots: false,
 						infinite: false,
 						speed: 500,
 						slidesToShow: 5,
-						slidesToScroll: 1,
-						draggable: false
+						slidesToScroll: 3,
+						draggable: false,
+                        nextArrow: RightCarouselButton,
+                        prevArrow: LeftCarouselButton,
 				    };
+
+                    var episodeItems = this.state.seasons[this.state.activeSeason].episodes.map(function(episode){
+                        var style = {
+                            backgroundImage: "url("+episode.screenshot+")",
+                            backgroundSize: "cover"
+                        };
+
+                        return (
+                            <div className="episode-item" key={episode.id}>
+                                <a href={this.linkForEpisode(episode)}>
+                                    <div style={style}>
+                                        <div className="inner-episode">
+                                            <i className="fa fa-2x fa-play-circle center--all"></i>
+                                            <span className="episode-text">Episode {episode.episodeNumber}</span>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        );
+                    }.bind(this));
 
                 	var episodeElem = (
                 		<Carousel {...settings}>
-							<div><h3>1</h3></div>
-							<div><h3>2</h3></div>
-							<div><h3>3</h3></div>
-							<div><h3>4</h3></div>
-							<div><h3>5</h3></div>
-							<div><h3>6</h3></div>
-							<div><h3>7</h3></div>
-							<div><h3>8</h3></div>
+							{episodeItems}
 						</Carousel>
                 	);
                 }else{
                 	var episodeElem = (
-                		<span className="no-seasons">
+                		<span className="no-seasons center--all">
                 			No Seasons Available Yet :(	
                 		</span>
                 	);
@@ -78,16 +135,17 @@ define(['react', 'jquery', 'app/controllers/home/ModalController', 'app/dependen
                         </a>
                         <ModalController isOpen={this.state.modalOpen} hideModal={this.closeModal} styles={styles}>
                             <div className="media-modal show-modal">
-                                <img className="inline-poster" src={show.poster} />
-                                <h2 className="show-name">{show.name}</h2>
-                                <h3 className="show-year">{str}</h3>
-                                <hr/><br/>
-                                <span>Description:</span>
-                                <p className="show-description text--justify">
-                                    {show.description}
-                                </p>
-                                <br/><br/>
-                                <div className="center">
+                                <div className="padded-content">
+                                    <img className="inline-poster" src={show.poster} />
+                                    <h2 className="show-name">{show.name}</h2>
+                                    <h3 className="show-year">{str}</h3>
+                                    <hr/><br/>
+                                    <span>Description:</span>
+                                    <p className="show-description text--justify">
+                                        {show.description}
+                                    </p>
+                                </div>
+                                <div className="show-row center">
                                     {episodeElem}
                                 </div>
                             </div>
