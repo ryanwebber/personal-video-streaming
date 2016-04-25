d101fb317481dd3f68f214bee0dafc16249c6524
Ryan Webber <ryan@sequence.com>
2016-04-25 13:01:57 -0700

    Added seperate watch page

---
+++ assets/js/controllers/watch/VideoController.jsx
@@ -0,0 +1,38 @@
+define(['react', 'jquery'],
+    function (React, $) {
+
+        var VideoController = React.createClass({
+            getInitialState: function(){
+                return {
+			position: 0,
+			loaded: false
+                };
+            },
+            videoLoaded: function(){
+		this.setState({
+			loaded: true
+		});
+		console.log(("hi"));
+            },
+            componentDidMount: function() {
+		this.props.video.onloadeddata = this.videoLoaded;
+            },
+            componentWillUnmount: function(){
+
+            },
+            render: function(){
+		if(!this.state.loaded){
+					return (
+				<div className="center--all">
+					<div className="spinner"></div>
+				</div>
+			);
+		}else{
+			return null;
+		}
+            }
+        });
+        return VideoController;
+
+    }
+);
